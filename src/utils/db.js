fe94b1f785cfbab64f7090c7ec68afb23e9caec2
ArathiJayaprakash <arathiparambil@gmail.com>
2023-10-20 12:24:08 +0530

    playwright test

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,3 +1,16 @@
 export const BASE_URL="http://localhost:3001"
 
 export const cardsData ="http://localhost:3001/cardsData"
+
+// {
+//     "id": "1",
+//     "imageSrc": "https://img.freepik.com/free-vector/general-business-horizontal-banner-template_23-2148958454.jpg?w=740&t=st=1697778108~exp=1697778708~hmac=b96582b452ec491549c630030f1d7316499c09057a970b14418a850ce73fd3d4",
+//     "name": "Creative Business",
+//     "status": "active"
+//   },
+//   {
+//     "id": "2",
+//     "imageSrc": "https://img.freepik.com/premium-vector/modern-business-flyer-design-template-brochure-cover_420373-29.jpg?w=740",
+//     "name": "Digital Marketing Agency",
+//     "status": "active"
+//   },
--- tests/signin.spec.js
+++ tests/signin.spec.js
@@ -5,19 +5,41 @@ test("signin test", async ({ page }) => {
   const emailField = page.locator("[name='email']");
   const passwordField = page.locator("[name='password']");
   const signInButton = page.locator("[type='submit']");
-
+  const editButton = page.locator("(//button[normalize-space()='Edit'])[1]");
+  const cardName = page.locator("[name='name']");
+  const updateButton = page.locator("//button[normalize-space()='Update']");
+  const removeButton = page.locator(
+    "(//button[normalize-space()='Remove'])[1]"
+  );
+  const deleteCardButton = page.locator("//button[normalize-space()='Delete']");
+  const signOutButton = page.locator("//button[normalize-space()='Sign out']");
 
   await page.goto("http://localhost:3000/");
   await page.waitForTimeout(3000);
   await signInLink.click();
   await emailField.fill("admin@gmail.com");
-  await passwordField.fill("admin@123");
-  await signInButton.click();
 
+  await passwordField.fill("admin@123");
+  await page.waitForTimeout(3000);
+
+  await signInButton.click();
 
   await page.waitForTimeout(3000);
 
+  await editButton.click();
+  await page.waitForTimeout(3000);
 
+  await cardName.fill("CIBC-Banner-$50");
+  await page.waitForTimeout(3000);
+
+  await updateButton.click();
+  await page.waitForTimeout(3000);
+
+  await removeButton.click();
+  await page.waitForTimeout(3000);
+
+  await deleteCardButton.click();
+  await page.waitForTimeout(3000);
+
+  await signOutButton.click();
 });
-
-
