3d6d16391d6ca247208b21739a70306525dab3b0
ArathiJayaprakash <arathiparambil@gmail.com>
2023-10-20 14:50:34 +0530

    ..

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,20 +3,30 @@ export const BASE_URL="http://localhost:3001"
 export const cardsData ="http://localhost:3001/cardsData"
 
 // {
-//     "id": "1",
-//     "imageSrc": "https://img.freepik.com/free-vector/general-business-horizontal-banner-template_23-2148958454.jpg?w=740&t=st=1697778108~exp=1697778708~hmac=b96582b452ec491549c630030f1d7316499c09057a970b14418a850ce73fd3d4",
-//     "name": "Creative Business",
-//     "status": "active"
-//   },
-//   {
-//     "id": "2",
-//     "imageSrc": "https://img.freepik.com/premium-vector/modern-business-flyer-design-template-brochure-cover_420373-29.jpg?w=740",
-//     "name": "Digital Marketing Agency",
-//     "status": "active"
-//   },
-// {
-//     "id": "4",
-//     "imageSrc": "https://img.freepik.com/free-psd/meditation-feeling-peace-banner-template_23-2148558519.jpg",
-//     "name": "Sunscreen product-square",
-//     "status": "active"
-//   },
+//     "cardsData": [
+//       {
+//         "id": "1",
+//         "imageSrc": "https://img.freepik.com/free-vector/general-business-horizontal-banner-template_23-2148958454.jpg?w=740&t=st=1697778108~exp=1697778708~hmac=b96582b452ec491549c630030f1d7316499c09057a970b14418a850ce73fd3d4",
+//         "name": "Creative Business",
+//         "status": "active"
+//       },
+//       {
+//         "id": "2",
+//         "imageSrc": "https://img.freepik.com/free-psd/fashion-designer-banner-template_23-2148856328.jpg?t=st=1697784424~exp=1697785024~hmac=204a1bcca83409cabd0b3340673426ff799b133114e93fc3f22b017694e3ae42",
+//         "name": "Digital Marketing Agency",
+//         "status": "active"
+//       },
+//       {
+//         "id": "3",
+//         "imageSrc": "https://img.freepik.com/free-vector/general-business-landing-page-template_23-2148958451.jpg?t=st=1697774982~exp=1697775582~hmac=6aadc099934724a777ebd22af8f9ff13dc5158b21625163c2de11c42a7784b41",
+//         "name": "2021 Business",
+//         "status": "active"
+//       },
+//       {
+//         "id": "5",
+//         "imageSrc": "https://img.freepik.com/free-psd/healthy-life-concept-banner-design_23-2148583499.jpg?w=826&t=st=1697792779~exp=1697793379~hmac=5efb473ecabfdd2487266fa36a5d3ff271d538bc48657d84286299b9896f3926",
+//         "name": "Free Vectors & PSD",
+//         "status": "inactive"
+//       }
+//     ]
+//   }
