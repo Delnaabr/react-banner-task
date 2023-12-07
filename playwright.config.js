ffd0d0ea2679a7504a7cf1204e437d3515b362e9
ArathiJayaprakash <arathiparambil@gmail.com>
2023-12-07 20:33:44 +0530

    updated

--- playwright.config.js
+++ playwright.config.js
@@ -33,9 +33,10 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-headless:false,
+  headless:false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot:'on'
   },
 
   /* Configure projects for major browsers */
